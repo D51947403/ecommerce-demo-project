29155409d18e25ab32fdfc57768908f85f933e97
D51947403 <singraul20codeblog@gmail.com>
2023-08-24 16:15:32 +0530

    ecommerce project demo

---
+++ spring-boot-ecommerce/src/main/java/com/singraul/ecommerce/TamasEmartApplication.java
@@ -0,0 +1,13 @@
+package com.singraul.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamasEmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamasEmartApplication.class, args);
+	}
+
+}
---
+++ spring-boot-ecommerce/src/test/java/com/singraul/ecommerce/TamasEmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singraul.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamasEmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
