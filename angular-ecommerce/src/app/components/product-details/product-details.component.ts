d3187d565140f8ba1bf37d910201a367e8cb6869
D51947403 <singraul20codeblog@gmail.com>
2023-09-06 16:08:04 +0530

    Implementing product details html page

--- angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -9,10 +11,11 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+
   //non-null assertion operator to avoid compiler warning
-  product!: Product;
+  public product!: Product;
   theProductId!: number;
-  constructor(private productService: ProductService, private route: ActivatedRoute) {
+  constructor(private productService: ProductService, private cartService:CartService, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
@@ -35,5 +38,9 @@ export class ProductDetailsComponent implements OnInit {
       }
     );
   }
-
+  addToCart() {
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
+    }
 }
