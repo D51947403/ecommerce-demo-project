bee10b8a128a5030dbfca02c0ffb525ec7c53866
D51947403 <singraul20codeblog@gmail.com>
2023-09-07 12:42:39 +0530

    checkout component and customer form group

---
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent  implements OnInit{
+
+   checkoutFormGroup!: FormGroup;
+
+   constructor(private formBuilder :FormBuilder){}
+
+  ngOnInit(): void {
+     this.checkoutFormGroup =this.formBuilder.group(
+      {
+        customer :this.formBuilder.group(
+          {
+          firstName :[''],
+          lastName :[''],
+          email :['']
+          })
+      });
+  }
+
+onSubmit(){
+  console.log('Handling the submit button');
+  console.log(this.checkoutFormGroup.get('customer')?.value);
+  console.log("The first name is : "+this.checkoutFormGroup.get('customer')?.value.firstName);
+  console.log("The last name is : "+this.checkoutFormGroup.get('customer')?.value.lastName);
+  console.log("The email address is : "+this.checkoutFormGroup.get('customer')?.value.email);
+
+}
+
+}
